8f94e2da4754242df3957cc75be6ff0d95b0204f
Istatillo <istatillojorayev@gmail.com>
2022-09-30 10:23:39 +0500

    third commit

--- controllers/sendMessage.js
+++ controllers/sendMessage.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 
 const sendMessage = (req, res) => {
-  let botId = "5248005861:AAG1tkGWe5C8THhXjsy7dKuPbMO5CYBiSc8";
-  let chatId = "1285249707";
+  let botId = process.env.BOT_ID;
+  let chatId = process.env.CHAT_ID;
   const { message } = req.body;
   axios
     .post("https://api.telegram.org/bot" + botId + "/sendMessage", {
